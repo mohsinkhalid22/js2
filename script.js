cec696b2142c3ce0cde797ed3fc9774ca53e15f3
mohsinkhalid22 <mohsinu2gud@gmail.com>
2024-08-05 11:52:08 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+/*This is a comment.
+This is a code that 
+prints hello world.*/
+console.log('hello World');
+
+//Arithmetic Operators
+
+let a = 7
+let b = 6
+
+console.log("a =", a, "b =", b);
+console.log('a + b =', a + b );
+console.log('a - b =', a - b );
+console.log('a x b =', a * b );
+console.log('a / b =', a / b ); 
+console.log('a % b =', a % b ); // Modulus Operator
+console.log('a ** b =', a ** b ); // Exponential Operator - 7^6 
+
+// Unary Operators
+
+let x = 5
+let y = 3
+
+console.log("x =", x, "y =",y);
+
+
+console.log("x++ =",x++); // Post Increment
+console.log(x);
+
+console.log("++x =",++x);
+
+console.log("--y =",--y);
+console.log("y++=",y++);
+console.log(y);
+
+/* console.log("++a =", ++a);
+console.log("--a =", --a); */
+
+let q = 10
+
+
+//q += 5 // q = q + 5
+//q -= 5 // q = q - 5
+//q *= 5 // q = q * 5
+q /= 5 // q = q / 5
+
+console.log("q =",q);
+
+// Comparison Operators
+
+let o = 6
+let i = 6
+
+console.log("o == i", o == i);
+console.log("o != i", o != i);
+console.log("o === i", o === i);
+console.log("o !== i", o !== i);
+console.log("6 < 6", o < i);
+console.log("6 <= 6", o <= i);
+
+//Logical Operators
+
+let t = 7
+let r = 6
+
+let cond1 = t === r;
+let cond2 = t < r;
+
+console.log("cond1 && cond2", cond1 && cond2);
+
+
+
